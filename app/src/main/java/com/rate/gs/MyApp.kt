a7ff9b7cb5e4443fd4d4ef28bc17970cb0de337a
Tuấn Anh <vtabk2@gmail.com>
2025-04-21 16:58:51 +0700

    update rate

--- app/src/main/java/com/rate/gs/MyApp.kt
+++ app/src/main/java/com/rate/gs/MyApp.kt
@@ -7,6 +7,6 @@ class MyApp: Application() {
     override fun onCreate() {
         super.onCreate()
         RateInApp.instance.registerActivityLifecycle(this)
-        RateInApp.instance.isRateGravityBottom = true
+        RateInApp.instance.isRateGravityBottom = false
     }
 }
